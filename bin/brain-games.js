1a7d40a08f986ea7f7cb3eb598acd7d611e55052
Dmitriy <3815300@gmail.com>
2022-03-24 10:50:50 +0300

    add github_action

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import {toGreet} from '../src/cli.js';
+import toGreet from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 toGreet();
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const toGreet = () => {
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${name}!`);
+};
 
-    const name = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${name}!`);
-}
-
-export {toGreet};
+export default toGreet;
